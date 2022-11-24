8dc106513a8063ee40d0c8cd46683538cb305265
Darshan Gajera <dennygajera@gmail.com>
2022-11-24 08:19:47 +0000

    Initial Commit

---
+++ gifDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gifDemo
+//
+//  Created by Darshan Gajera on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
